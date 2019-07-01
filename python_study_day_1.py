7f8b2ae7214013bfd1580dfc6089203341dd092d
alwayschoi <sangjochoi@gmail.com>
2019-07-01 22:29:09 +0900

    작심삼일 day 1

---
+++ python_study_day_1.py
@@ -0,0 +1,12 @@
+Python 3.7.3 (v3.7.3:ef4ec6ed12, Mar 25 2019, 21:26:53) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 13+2-5
+10
+>>> print("hello?")
+hello?
+>>> import turtle as t
+>>> t.shape("turtle")
+>>> t.forward(50)
+>>> t.right(90)
+>>> t.forward(50)
+>>> 
